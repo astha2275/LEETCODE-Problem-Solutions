78c597d7864a3dd98a2f8ed50bc8f8b6657db448
astha <asthatripathii404@gmail.com>
2025-07-15 22:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.27%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   double myPow(double x, int n) {
+    long long n2 = n;         // handle negative n safely
+    double result = 1;
+
+    if (n2 < 0) {
+        x = 1 / x;
+        n2 = -n2;
+    }
+
+    while (n2 > 0) {
+        if (n2 % 2 == 1) {
+            result *= x;
+        }
+        x *= x;
+        n2 /= 2;
+    }
+
+    return result;
+}
+
+};

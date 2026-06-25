5f204fc03e40d7479436eba507c8f491ac078a10
astha <asthatripathii404@gmail.com>
2026-06-25 21:40:00 +0530

    Time: 7 ms (21.77%) | Memory: 68.4 MB (32.24%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.ts
@@ -0,0 +1,9 @@
+function maxTotalValue(nums: number[], k: number): number {
+    let m1 = Infinity,
+        m2 = -Infinity;
+    for (const x of nums) {
+        m1 = Math.min(m1, x);
+        m2 = Math.max(m2, x);
+    }
+    return (m2 - m1) * k;
+}
